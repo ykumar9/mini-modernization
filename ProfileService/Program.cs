35a7143dc1be18c67e94b7eae41f4fa5a97045d5
ykumar9 <Yogesh.kumar1@wipro.com>
2025-08-07 16:38:33 +0000

    Trigger deploy

--- ProfileService/Program.cs
+++ ProfileService/Program.cs
@@ -42,3 +42,4 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+// trigger ProfileService deployment
